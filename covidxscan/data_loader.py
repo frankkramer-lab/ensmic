abb7823fc565af24dc599661cd9e1fa7556c2493
muellerdo <dominik.mueller@informatik.uni-augsburg.de>
2020-03-26 20:13:54 +0100

    Functional data loader including test environment for debugging

--- covidxscan/data_loader.py
+++ covidxscan/data_loader.py
@@ -102,19 +102,43 @@ print("All samples: " + str(sample_list))
 # Library import
 from miscnn import Preprocessor
 
+from subfunctions.sf_resize import Resize
+from subfunctions.sf_class import SegFix
+
 # Create and configure the Preprocessor class
-pp = Preprocessor(data_io, data_aug=None, batch_size=1, prepare_subfunctions=True,
-                  prepare_batches=False, analysis="fullimage")
+pp = Preprocessor(data_io, data_aug=None, batch_size=1,
+                  subfunctions=[SegFix(), Resize()],
+                  prepare_subfunctions=True,
+                  prepare_batches=False,
+                  analysis="fullimage")
+
+
+# testing data generator
+# from miscnn.neural_network.data_generator import DataGenerator
+# dataGen = DataGenerator(sample_list, pp, training=True,
+#                         validation=False, shuffle=False)
+#
+# for img,seg in dataGen:
+#     print(img.shape)
+#     print(seg, seg.shape)
 
 # Library import
 from miscnn.neural_network.model import Neural_Network
-from keras.metrics import sparse_categorical_accuracy
+from keras.metrics import categorical_crossentropy
 
 from model import Architecture
 
+# Define input shape
+input_shape = (224, 224, 1)
+
 # Create the Neural Network model
-model = Neural_Network(preprocessor=pp, loss=sparse_categorical_accuracy,
-                       architecture=Architecture(), metrics=[])
+model = Neural_Network(preprocessor=pp, loss=categorical_crossentropy,
+                       architecture=Architecture(input_shape), metrics=[])
+
+# model.model.summary()
 
 # Train
 model.train(sample_list[:50], epochs=5)
+
+# lol = model.model.predict(img)
+# print(lol, lol.shape)
