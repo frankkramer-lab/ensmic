1d98ccbc6bb03a95ff20a1c763d64d8fc7617a1e
muellerdo <dominik.mueller@informatik.uni-augsburg.de>
2020-03-27 19:04:08 +0100

    Prepare covidxscan setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
    long_description_content_type="text/markdown",
    packages=find_packages(),
    install_requires=['pandas',
-                     'miscnn',
+                     #'miscnn',
                      'Pillow'],
    classifiers=["Programming Language :: Python :: 3",
                 "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
