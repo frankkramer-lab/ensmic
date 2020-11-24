1c881881006cc435a422955617c12363b6e27476
muellerdo <domey038@live.de>
2020-11-24 18:34:41 +0100

    Implemented improved EarlyStopping Callback

---
+++ ensmic/utils/callbacks.py
@@ -0,0 +1,51 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+#-----------------------------------------------------#
+#                   Custom Callbacks                  #
+#-----------------------------------------------------#
+"""
+Author:   JBSnorro
+Source:   https://stackoverflow.com/questions/53500047/stop-training-in-keras-when-accuracy-is-already-1-0
+
+Changed baseline to act as a real baseline.
+The number of patience epochs are only counted when baseline loss is achieved.
+"""
+class ImprovedEarlyStopping(EarlyStopping):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.baseline_attained = False
+
+    def on_epoch_end(self, epoch, logs=None):
+        if not self.baseline_attained:
+            current = self.get_monitor_value(logs)
+            if current is None:
+                return
+            if self.monitor_op(current, self.baseline):
+                if self.verbose > 0:
+                    print('Baseline attained.')
+                self.baseline_attained = True
+            else:
+                return
+        super(MyEarlyStopping, self).on_epoch_end(epoch, logs)
