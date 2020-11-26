79d77562edd63debe4f7df29efc64e633dca9185
muellerdo <domey038@live.de>
2020-11-26 12:38:00 +0100

    Reworked phase two script structure

--- ensmic/phase_ii/train_inf.py
+++ ensmic/phase_ii/train_inf.py
@@ -23,9 +23,10 @@
 import argparse
 import os
 import time
+import json
+import pandas as pd
 # Internal libraries/scripts
 from ensmic.ensemble import ensembler_dict, ensembler
-from ensmic.architectures import architectures
 
 #-----------------------------------------------------#
 #                      Argparser                      #
@@ -42,16 +43,12 @@ args = parser.parse_args()
 #-----------------------------------------------------#
 # Initialize configuration dictionary
 config = {}
-# Path to data directory
-config["path_data"] = "data"
 # Path to result directory
 config["path_results"] = "results"
 # Seed (if training multiple runs)
 config["seed"] = args.seed
 # List of ensemble learning techniques
 config["ensembler_list"] = ensembler
-# List of architectures which inferences should be included into ensembling
-config["architecture_list"] = architectures # all architectures
 
 # Adjust possible classes
 if config["seed"] == "x-ray":
@@ -63,70 +60,30 @@ else:
     pass
 
 #-----------------------------------------------------#
-#            Prepare Result File Structure            #
-#-----------------------------------------------------#
-def prepare_rs(config):
-    # Create results directory
-    if not os.path.exists(config["path_results"]):
-        os.mkdir(config["path_results"])
-    # Create subdirectories for phase
-    path_phase = os.path.join(config["path_results"],
-                              "phase_ii" + "." + str(config["seed"]))
-    if not os.path.exists(path_phase) : os.mkdir(path_phase)
-    # Create subdirectories for ensemble learning methods
-    for elm in config["ensembler_list"]:
-        path_elm = os.path.join(path_phase, elm)
-        if not os.path.exists(path_elm) : os.mkdir(path_elm)
-    # Combine inferences from phase one
-    path_poinf = os.path.join(path_phase, "phase_i.inference")
-    if not os.path.exists(path_poinf) : os.mkdir(path_poinf)
-    arch_list = []
-    inf_val = {}
-    inf_test = {}
-    # Iterate over all architectures
-    for arch in config["architecture_list"]:
-        # Identify pathes
-        path_arch = os.path.join(config["path_results"],
-                                 "phase_i" + "." + str(config["seed"]),
-                                 arch)
-        path_arch_val = os.path.join(path_arch, "inference." + \
-                                     "val-model" + ".json")       # DEBUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUGGGGGGING
-        path_arch_test = os.path.join(path_arch, "inference." + \
-                                      "test" + ".json")
-        try:
-            # Load predictions on samples for val-ensemble subset
-            infIO = IO_Inference(config["class_dict"], path=path_arch_val)
-            inf_val[arch] = infIO.load_inference()
-            # Load predictions on samples for test subset
-            infIO = IO_Inference(config["class_dict"], path=path_arch_test)
-            inf_test[arch] = infIO.load_inference()
-            # Cache architecture with available inference
-            arch_list.append(arch)
-        except Exception as e:
-            print("Skipping inference of architecture:", arch)
-            print(arch, str(e))
-    # Create datasets from inference
-    create_dataset(inf_val, inf_test, config)
-    # Return list of usable architectures
-    return arch_list
-
-#-----------------------------------------------------#
-#                   Create dataset                    #
-#-----------------------------------------------------#
-def create_dataset(inf_val, inf_test, config):
-    print(inf_val)
-
-#-----------------------------------------------------#
 #                     Run Training                    #
 #-----------------------------------------------------#
-def run_training(ensembler, arch_list, config):
-    print(ensembler, arch_list)
+def run_training(ds_x, ds_y, ensembler, path_phase, config):
+    # Create model
+    model = ensembler_dict[ensembler]()
+    # Fit model on data
+    model.training(ds_x, ds_y)
+    # Dump fitted model to disk
+    path_elm = os.path.join(path_phase, ensembler, "model.pkl")
+    model.dump(path_elm)
 
 #-----------------------------------------------------#
 #                     Main Runner                     #
 #-----------------------------------------------------#
-# Setup file structure for results directory
-arch_list = prepare_rs(config)
+# Identify phase results directory
+path_phase = os.path.join(config["path_results"],
+                          "phase_ii" + "." + str(config["seed"]))
+# Load dataset for training
+ds_x = pd.read_csv(os.path.join(path_phase, "phase_i.inference." + \
+                                "val-ensemble." + "data" + ".csv"),
+                   header=0, index_col="index")
+ds_y = pd.read_csv(os.path.join(path_phase, "phase_i.inference." + \
+                                "val-ensemble." + "class" + ".csv"),
+                   header=0, index_col="index")
 
 # Initialize cache memory to store meta information
 timer_cache = {}
@@ -137,7 +94,7 @@ for ensembler in config["ensembler_list"]:
     try:
         # Run Fitting Pipeline
         timer_start = time.time()
-        run_training(ensembler, arch_list, config)
+        run_training(ds_x, ds_y, ensembler, path_phase, config)
         timer_end = time.time()
         # Store execution time in cache
         timer_time = timer_end - timer_start
@@ -146,8 +103,8 @@ for ensembler in config["ensembler_list"]:
     except Exception as e:
         print(ensembler, "-", "An exception occurred:", str(e))
 
-# # Store time measurements as JSON to disk
-# path_time = os.path.join(config["path_results"], "phase_i" + "." + \
-#                          config["seed"], "time_measurements.json")
-# with open(path_time, "w") as file:
-#     json.dump(cache, file, indent=2)
+# Store time measurements as JSON to disk
+path_time = os.path.join(config["path_results"], "phase_ii" + "." + \
+                         config["seed"], "time_measurements.json")
+with open(path_time, "w") as file:
+    json.dump(timer_cache, file, indent=2)
