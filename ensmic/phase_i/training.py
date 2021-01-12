50ec9ad5bee144a4b9c64a4a84c6cdb40f2e606d
muellerdo <domey038@live.de>
2021-01-12 23:28:22 +0100

    Adjusted data augmentation configuration

--- ensmic/phase_i/training.py
+++ ensmic/phase_i/training.py
@@ -103,17 +103,19 @@ def setup_miscnn(architecture, sf_normalization, config):
 
     # Create Data Augmentation class
     data_aug = Data_Augmentation(cycles=1, scaling=True, rotations=True,
-                                 elastic_deform=False, mirror=True,
-                                 brightness=True, contrast=True,
-                                 gamma=True, gaussian_noise=False)
+                                 elastic_deform=True, mirror=True,
+                                 brightness=False, contrast=True,
+                                 gamma=False, gaussian_noise=True)
     # Configure Data Augmentation
     data_aug.seg_augmentation = False
-    data_aug.config_p_per_sample = 0.10
+    data_aug.config_p_per_sample = 0.15
     data_aug.config_mirror_axes = (0, 1)
-    data_aug.config_contrast_range = (0.8, 1.2)
-    data_aug.config_brightness_range = (0.8, 1.2)
-    data_aug.config_gamma_range = (0.8, 1.2)
     data_aug.config_scaling_range = (0.8, 1.2)
+    data_aug.config_elastic_deform_alpha = (0.0, 200.0)
+    data_aug.config_elastic_deform_sigma = (9.5, 10.5)
+    data_aug.config_contrast_range = (0.9, 1.1)
+    data_aug.coloraug_per_channel = False
+    data_aug.config_contrast_preserverange = True
 
     # Initialize architecture of the neural network
     nn_architecture = architecture_dict[architecture](config["channels"])
