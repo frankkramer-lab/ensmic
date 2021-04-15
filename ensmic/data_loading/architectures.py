53a9fc9dc04b6566efa8958fe78b3aa9f39660ae
muellerdo <domey038@live.de>
2021-04-15 17:41:02 +0200

    Added architecture list config to data loading

---
+++ ensmic/data_loading/architectures.py
@@ -0,0 +1,45 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+architecture_list = [
+    'Vanilla',
+    'ResNet50',
+    'ResNet101',
+    'ResNet152',
+    'ResNet50V2',
+    'ResNet101V2',
+    'ResNet152V2',
+    'ResNeXt50',
+    'ResNeXt101',
+    'DenseNet121',
+    'DenseNet169',
+    'DenseNet201',
+    'EfficientNetB0',
+    'EfficientNetB1',
+    'EfficientNetB2',
+    'EfficientNetB3',
+    'InceptionResNetV2',
+    'InceptionV3',
+    'MobileNet',
+    'MobileNetV2',
+    'NASNetMobile',
+    'NASNetLarge',
+    'VGG16',
+    'VGG19',
+    'Xception'
+    ]
