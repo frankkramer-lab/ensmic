Enhanced macro-averaging for ROC curves: Fixes #52

ensmic/utils/categorical_averaging.py
@@ -22,6 +22,7 @@
 # External libraries
 import numpy as np
 import os
+import pandas as pd
 # Internal libraries/scripts
 
 #-----------------------------------------------------#
@@ -38,8 +39,23 @@ def macro_averaging(results, dataset, path_eval, index_col="architecture"):
     macro_averaged.to_csv(path_res, index=False)
     return macro_averaged
 
-#-----------------------------------------------------#
-#              Function: Micro Averaging              #
-#-----------------------------------------------------#
-def micro_averaging():
-    pass
+# Macro-Average AUROC scores
+def macro_average_roc(group):
+    # First aggregate all false positive rates
+    all_fpr = np.unique(group["FPR"])
+    mean_tpr = np.zeros_like(all_fpr)
+
+    # Then interpolate all ROC curves at this points
+    class_list = np.unique(group["class"])
+    fpr_list = [group[group["class"]==c]["FPR"] for c in class_list]
+    tpr_list = [group[group["class"]==c]["TPR"] for c in class_list]
+    for i in range(len(class_list)):
+        mean_tpr += np.interp(all_fpr, fpr_list[i], tpr_list[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= len(class_list)
+    fpr = all_fpr
+    tpr = mean_tpr
+
+    # Return resulting macro-averaged auroc scores
+    return pd.DataFrame({"FPR": fpr, "TPR": tpr})